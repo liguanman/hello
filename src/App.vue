d4c648824efef08be65b1b3ac9cef8047c2e146a
liguanman <610158119@qq.com>
2018-11-27 23:08:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <drawer :show-mode="drawerConfig.mode"
+            :placement="drawerConfig.placement"
+            :drawer-style="drawerConfig.style">
+      <div slot="drawer">
+      <!-- 菜单内容 -->
+      </div>
+      <div>
+        <router-view></router-view>
+      </div>
+    </drawer>
+    <tabbar>
+      <tabbar-item>
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item>
+        <span slot="label">Message</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Drawer, Tabbar } from 'vux'
+
+export default {
+  name: 'App',
+  components: {
+    Drawer,
+    Tabbar
+  },
+  data: () => ({
+    drawerConfig: {
+        mode: 'overlay',
+        placement: 'left',
+        style: {
+          width: '200px',
+          backgroundColor: '#35495e'
+        }
+      },
+  })
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">
+      <x-header>This is the page title.</x-header>
+  </div>
+</template>
+
+<script>
+import { XHeader } from 'vux'
+export default {
+    components: {
+      XHeader
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
